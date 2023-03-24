ecdd8b85e214d30c210a83862e9e8d598aeaafd0
Alem Haddush Fitwi <fitwialem2022@fb.com>
2023-03-24 05:20:56 -0700

    Update and rename pickling.py to pickling_json.py

--- pickling_json.py
+++ pickling_json.py
@@ -1,5 +1,6 @@
 # Module
 # Python pickle module is used for serializing and de-serializing python object structures
+import json
 import pickle
 import numpy as np
 
@@ -21,3 +22,23 @@ fp.close()
 
 # Unpickle Data On Memory
   unpickled_arr = pickle.loads(arr)
+   
+#-----------------------------------------------
+# Save Data To Disk As JSON
+dct = {"Alem":40}
+with open('datafile.json', 'w') as fp:
+   json.dump(dct, fp)
+fp.close(
+    
+# Read json Data From Disk Unpickled
+with open ("datafile.json", "r") as fp:
+  alem = json.load(fp)
+fp.close()
+
+# Convert dict to json On Memory
+  dct = {"Alem":40}
+  jsondata = pickle.dumps(arr)
+
+# Convert json to dict On Memory
+  dct = pickle.loads(jsondata)
+#-----------------------------------------------
