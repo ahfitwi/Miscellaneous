bbf982f35f8f26c86238781d78ebfa5d78a5ee52
Alem Haddush Fitwi <fitwialem2022@fb.com>
2023-05-17 09:26:14 -0700

    Create pysimplegui.py

---
+++ pysimplegui.py
@@ -0,0 +1,12 @@
+#https://www.youtube.com/watch?v=a3AeM1GZNTk
+#pip install PySimpleGui
+import PySimpleGui as psg
+psg.popup("HW")
+psg.popup_ok("HW")
+psg.popup_cancel("HW")
+psg.popup_ok_cancel("HW")
+psg.popup_yes_no("HW")
+psg.popup_auto_close("HW")
+psg.popup_timed("HW", auto_close_duration=6)
+psg.popup_error("HW")
+psg.popup_no_border("HW")
